ee71139415f18ac39ad4a03385a028a2412deb9a
Adam Sadiq <adamsadiq@eduroam-126-70.nomadic.bris.ac.uk>
2019-10-30 09:30:34 +0000

    removing 1f

--- Romi.ino
+++ Romi.ino
@@ -217,7 +217,7 @@ void loop(){
   executingCommand = true; // do not trigger commands above (global space)
 
   float measurement = (float) right_velocity;
-  float demand = 1f;
+  float demand = 1;
 
   unsigned long elapsed_time;
   elapsed_time = millis() - vel_update;
