dc64604bc60e85516873ce03ad4f4e3d3a1ffba9
Adam Sadiq <adamsadiq@eduroam-126-70.nomadic.bris.ac.uk>
2019-11-01 17:12:30 +0000

    changing demand to 0.15

--- Romi.ino
+++ Romi.ino
@@ -298,7 +298,7 @@ void loop(){
   float measurement_l = 0;
   float measurement_r = 0;
 
-  float demand = 0.3;
+  float demand = 0.15;
 
   measurement_l = left_velocity;
   measurement_r = right_velocity;
