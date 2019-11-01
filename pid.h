ac7188390595a5a0bacd42bb3520544c11724b60
Adam Sadiq <adamsadiq@eduroam-126-70.nomadic.bris.ac.uk>
2019-11-01 15:12:00 +0000

    updating pid class

--- pid.h
+++ pid.h
@@ -143,21 +143,21 @@ float PID::update(float demand, float measurement) {
   //This represents the error term
   // Decide what your error signal is (demand vs measurement)
   float error;
-  //error = ????;   
+  error = demand - measurement;
   
   //This represents the error derivative
   // Calculate the change in your error between update()
   float error_delta;
-  //error_delta = ????;
+  error_delta = (error - last_error)/time_delta;
 
   // This represents the error integral.
   // Integrate error over time.
-  //integral_error = ???;
+  integral_error *= time_delta;
 
   //Attenuate above error components by gain values.
-  //Kp_output = Kp * ????;
-  //Ki_output = Ki * ????;
-  //Kd_output = Kd * ????;
+  Kp_output = Kp * error;
+  Ki_output = Ki * integral_error;
+  Kd_output = Kd * error_delta;
 
   // Add the three components to get the total output
   // Note: Check the sign of your d gain.  Check that the
